44c3b5c355217e0df8f80b431b0b90236d5eab38
zbydonuts <zhu.bingyi@donuts.ne.jp>
2018-03-05 12:24:42 +0900

    fix interactive transition

--- AnimatorDismiss/Navigation/CViewController.swift
+++ AnimatorDismiss/Navigation/CViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class CViewController: UIViewController {
+    
+    var interactor: NaviInteractor?
 
     @IBOutlet weak var pushButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
+        interactor = NaviInteractor(attachTo: self)
         // Do any additional setup after loading the view.
     }
 
@@ -35,10 +36,61 @@ extension CViewController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         switch operation {
         case .push:
-            return NaviAnimator(duration: 1, isPresenting: true, originFrame: view.frame)
+            interactor = NaviInteractor(attachTo: toVC)
+            return NaviAnimator(duration: 0.3, isPresenting: true, originFrame: view.frame)
         case .pop, .none:
-            return NaviAnimator(duration: 1, isPresenting: false, originFrame: view.frame)
+            return NaviAnimator(duration: 0.3, isPresenting: false, originFrame: view.frame)
         }
     }
     
+    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        guard let ia = interactor else { return  nil }
+        return ia.transitionInProgress ? ia : nil
+    }
+}
+
+
+class NaviInteractor: UIPercentDrivenInteractiveTransition {
+    weak var navigationController: UINavigationController?
+    var transitionInProgress = false
+    var shouleCompleteTransition = false
+    
+    init?(attachTo viewController: UIViewController) {
+        if let nav = viewController.navigationController {
+            self.navigationController = nav
+            super.init()
+            setupBakcGesture(view: viewController.view)
+        } else {
+            return nil
+        }
+    }
+    
+    private func setupBakcGesture(view: UIView) {
+        let switpeBackGesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleBackGesture(_:)))
+        switpeBackGesture.edges = .left
+        view.addGestureRecognizer(switpeBackGesture)
+    }
+    
+    @objc private func handleBackGesture(_ gesture: UIScreenEdgePanGestureRecognizer) {
+        guard let navi = self.navigationController else { return }
+        let viewTransition = gesture.translation(in: gesture.view?.superview)
+        let progress = viewTransition.x / navi.view.frame.width
+        
+        switch gesture.state {
+        case .began:
+            transitionInProgress = true
+            navi.popViewController(animated: true)
+        case .changed:
+            shouleCompleteTransition = progress > 0.5
+            update(progress)
+        case .cancelled:
+            transitionInProgress = false
+            cancel()
+        case .ended:
+            transitionInProgress = false
+            shouleCompleteTransition ? finish() : cancel()
+        default:
+            break
+        }
+    }
 }
